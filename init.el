98864bed0860bd686384dbd19269714cf2bf236c
Giulio Scattolin <giulio.scattolin@gmail.com>
2023-10-02 12:51:53 +0200

    feat: avoid polluting init.el with customization information

---
+++ init.el
@@ -0,0 +1,4 @@
+;; Avoid polluting this file with customization information.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
