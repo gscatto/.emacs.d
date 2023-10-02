ce1a3a09c338c24ef99489bf4e233b6dd8dbeb31
Giulio Scattolin <giulio.scattolin@gmail.com>
2023-10-02 13:08:16 +0200

    feat: avoid polluting working directories with file backups

--- init.el
+++ init.el
@@ -2,3 +2,6 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
+
+;; Avoid polluting working directories with file backups.
+(setq backup-directory-alist `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
