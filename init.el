9a969e4fc2d1b1374cf7154fb1b066988dacece9
Giulio Scattolin <giulio.scattolin@gmail.com>
2024-04-19 09:54:50 +0200

    Install straight.el, a functional package manager for Emacs

--- init.el
+++ init.el
@@ -2,3 +2,39 @@
 ;;
 ;; See also https://www.emacswiki.org/emacs/AlarmBell.
 (setq visible-bell 1)
+
+;; Install straight.el, a next-generation, purely functional package
+;; manager for the Emacs hacker.
+;;
+;; See also https://github.com/radian-software/straight.el
+;;
+;; Configure straight.el before installation.
+;;
+;; Make each use-package form also invoke straight.el to install the
+;; package, unless otherwise specified.
+(setq straight-use-package-by-default t)
+;;
+;; Install it.
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+;;
+;; Install use-package with straight.el.
+(straight-use-package 'use-package)
+;;
+;; Install packages automatically if not already present on our
+;; system.  See also https://github.com/jwiegley/use-package.
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
