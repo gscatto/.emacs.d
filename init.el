4bb4007937371fe1f989b80a1a5e8f6d1dca9e97
Giulio Scattolin <giulio.scattolin@gmail.com>
2024-09-13 16:46:32 +0200

    Remap the Command key on macOS to Ctrl

---
+++ init.el
@@ -0,0 +1,8 @@
+;; Remap the Command key on macOS to Ctrl.
+;;
+;; Source: https://www.reddit.com/r/emacs/comments/isl1s5/remapping_the_command_key_on_macos_to_ctrl/
+(custom-set-variables
+ '(ns-alternate-modifier 'meta)
+ '(ns-right-alternate-modifier 'meta) 
+ '(ns-command-modifier 'control)
+ '(ns-right-command-modifier 'control))
